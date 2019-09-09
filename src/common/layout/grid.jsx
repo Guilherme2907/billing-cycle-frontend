6dda77e8866827d3a5be795be8223298a7616897
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2019-09-09 15:17:48 -0300

    Configuração inicial e criação dos componentes de layout

---
+++ src/common/layout/grid.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default props => {
+  const toCssClass = (numbers = "12") => {
+    const colunms = numbers.split(" ");
+    let classes = '';
+
+    if(colunms[0]) {classes += `col-xs-${colunms[0]} `}
+    if(colunms[1]) {classes += `col-sm-${colunms[1]} `}
+    if(colunms[2]) {classes += `col-md-${colunms[2]} `}
+    if(colunms[3]) {classes += `col-lg-${colunms[3]}`}
+
+    return classes;
+  };
+
+  const classe = toCssClass(props.cols); 
+  console.log(classe)
+  return (
+      <div className={classe}>
+          {props.children}
+      </div>
+  )
+};
---
+++ src/dashboard/dashboard.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import ContentHeader from "../common/template/contentHeader";
+import Content from "../common/template/content";
+import ValueBox from "../common/widget/valueBox";
+import Row from "../common/layout/row";
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title="DashBoard" subTitle="versão 1.0" />
+        <Content>
+          <Row>
+            <ValueBox
+              cols="12 4"
+              color="green"
+              icon="bank"
+              value="R$ 10"
+              text="Total de créditos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="red"
+              icon="credit-card"
+              value="R$ 5"
+              text="Total de débitos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="blue"
+              icon="money"
+              value="R$ 5"
+              text="Valor consolidado"
+            />
+          </Row>
+        </Content>
+      </div>
+    );
+  }
+}
